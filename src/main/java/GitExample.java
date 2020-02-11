6046885f71b33bac4889151e5354943bbc39343f
Metsis <hanna.metsis@gmail.com>
2020-02-11 13:21:07 +0100

    henriks main

--- src/main/java/GitExample.java
+++ src/main/java/GitExample.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
@@ -12,3 +13,104 @@ public class GitExample {
         terminal = terminalFactory.createTerminal();
     }
 }
+=======
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.util.Random;
+
+public class GitExample extends Canvas implements Runnable {
+
+
+
+        public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+
+        private Thread thread;
+        private boolean running = false;
+
+        private Random r;
+        private Handler handler;
+
+        public Game() {
+            new Window(WIDTH, HEIGHT, "The Game", this);
+
+            handler = new Handler();
+            r = new Random();
+
+            for(int i = 0; i< 50; i++) {
+                handler.addObject(new Player(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.Player));
+            }
+
+
+        }
+
+        public synchronized void start() {
+            thread = new Thread(this);
+            thread.start();
+            running = true;
+        }
+
+        public synchronized void stop() {
+            try {
+                thread.join();
+                running = false;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            long lastTime = System.nanoTime();
+            double amountsOfTicks = 60.0;
+            double ns = 1000000000 / amountsOfTicks;
+            double delta = 0;
+            long timer = System.currentTimeMillis();
+            int frames = 0;
+            while (running) {
+                long now = System.nanoTime();
+                delta += (now - lastTime) /ns;
+                lastTime = now;
+                while(delta>= 1) {
+                    tick();
+                    delta--;
+                }
+                if(running)
+                    render();
+                frames++;
+
+                if(System.currentTimeMillis() - timer > 1000){
+                    timer += 1000;
+                    System.out.println("FPS: " + frames);
+                    frames = 0;
+                }
+            }
+            stop();
+        }
+
+        private void tick() {
+            handler.tick();
+
+        }
+
+        private void render() {
+            BufferStrategy bs = this.getBufferStrategy();
+            if(bs == null){
+                this.createBufferStrategy(3);
+                return;
+            }
+
+            Graphics g = bs.getDrawGraphics();
+
+            g.setColor(Color.black);
+            g.fillRect(0, 0, WIDTH, HEIGHT);
+
+            handler.render(g);
+
+            g.dispose();
+            bs.show();
+        }
+
+        public static void main(String[] args) {
+            new Game();
+        }
+    }
+>>>>>>> aa3e4d46460d17bda31472218657d964d8f21de5
