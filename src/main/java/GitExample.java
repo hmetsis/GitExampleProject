0857de1833e9f1b54e61a6506e46034f647c4425
Metsis <hanna.metsis@gmail.com>
2020-02-11 11:54:01 +0100

    terminal

--- src/main/java/GitExample.java
+++ src/main/java/GitExample.java
@@ -1,6 +1,15 @@
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class GitExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        Terminal terminal;
+        boolean continuereadinginput = true;
+        terminal = terminalFactory.createTerminal();
         
     }
 
